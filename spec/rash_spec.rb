1e544af54f80bf1f8b0d9cd26563bf1986304bd9
Pcboy <david.hagege@gmail.com>
2012-04-20 23:01:58 +0900

    Fix test.

--- spec/rash_spec.rb
+++ spec/rash_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe RHash do
+describe Rash do
 
   it "can insert elements and generate random key" do
-    h = RHash.new
+    h = Rash.new
     h << '42'
-    h.keys.first.length.should == RHash::UUIDLEN
+    h.keys.first.length.should == Rash::UUIDLEN
     h.values.first.should == '42'
   end
 
